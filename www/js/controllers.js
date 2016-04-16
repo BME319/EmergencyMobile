1d081ef073346d2beb091efd7b44cb49f0489b3f
Jiazhen Xiong <Jiazhen Xiong>
2016-04-16 16:16:33 +0800

    111

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -334,94 +334,9 @@ angular.module('controllers', ['ionic','ngResource','services'])
     };
   $scope.select = function(item) {
     item.itemClass = !item.itemClass;
-  };
-  //从生理设备获取数据
-  var receivevdata = function(i,bluetoothdevice){
-    //$scope.testdata = [];
-    if(ionic.Platform.platform()!='win32' && bluetoothdevice!=undefined && bluetoothdevice!="")
-    {
-      //var bluetoothdevice = angular.fromJson(window.localStorage['bluetoothdeviceP']);
-      ble.connect(bluetoothdevice.substr(0,17),
-        function(s)
-        {
-          //console.log(s);
-          // console.log(s.id+" "+s.services[2]+" "+s.characteristics[6].characteristic);
-          ble.read(s.id, s.services[2], s.characteristics[6].characteristic,
-            function(buff){
-              console.log(buff);
-              console.log('buff');
-              var data = new Uint8Array(buff);
-              ///////////获取成功进行存储，此处数据格式尚待确定
-              $rootScope.$apply(function(){
-                $scope.PatientList[i]['heartRate']=data[4];
-                $scope.PatientList[i]['temperature']=data[7]+data[8]/100;
-                // $scope.catalog.Physical[0].value = data[7]+data[8]/100;//体温
-                // $scope.catalog.Physical[1].value = data[4];//脉率
-                // $scope.catalog.Physical[3].value = data[5];//收缩压
-                // $scope.catalog.Physical[4].value = data[6];//舒张压
-              })
-              console.log(data);
-              ///断开连接
-              ble.disconnect(s.id,
-                function(dcsuccess){
-                  console.log(dcsuccess)
-                },
-                function(dce){
-                  console.log(dce);
-                });
-            },function(e){
-              console.log(e);
-               ///断开连接
-              ble.disconnect(s.id,
-                function(dcsuccess){
-                  console.log(dcsuccess)
-                },
-                function(dce){
-                  console.log(dce);
-                });
-            });
-        },function(e)
-        {
-          console.log(e);
-           ///断开连接
-              ble.disconnect(s.id,
-                function(dcsuccess){
-                  console.log(dcsuccess)
-                },
-                function(dce){
-                  console.log(dce);
-                });
-        });
-      console.log(bluetoothdevice);
-    }
-  }
-  $scope.bleRefresh = function(){
-    $ionicLoading.show({template: '开始搜索附近设备', duration:1500 });
-    for(var i in $scope.PatientList){
-      receivevdata(i,$scope.PatientList[i].TerminalIP);
-    }
-  } 
+  }; 
   //根据状态获取不同列表，并控制显示
-  var GetPatientsbyStatus = function(Status)
-  {
-      var promise = PatientVisitInfo.GetPatientsbyStatus(Status); 
-      promise.then(function(data)
-      { 
-         for(var i=0;i<data.length;i++){
-          data[i].itemClass=false;
-          data[i].bleData={
-            heartRate:"",
-            temperature:""
-          };
-          if(Status==1){
-            receivevdata(i,t);
-          }
-         }
-         $scope.PatientList = data;
-         $scope.$broadcast('scroll.refreshComplete'); 
-        },function(err) {   
-      });      
-  }             
+          
   //不同角色，列表跳转不同
   $scope.setCurrent = function(item) {
     Storage.set("PatientID", item.PatientID);  
@@ -481,39 +396,7 @@ angular.module('controllers', ['ionic','ngResource','services'])
   //tab选中的控制
   //默认显示 初始化与角色权限相关
   if(Storage.get('RoleCode')=='EmergencyPersonnel'){    
-      $scope.tab1_checked=true;  
-      $scope.tab2_checked=false;  
-      $scope.tab3_checked=false;  
-      $scope.curtab="tab1";  
-      $scope.tab1_color={color:'blue'};   
-      $scope.tab2_color="";  
-      $scope.tab3_color="";  
-      GetPatientsbyStatus(1);
-      $scope.newPatientIcon=true;  
-  }
-  else if(Storage.get('RoleCode')=='DividePersonnel'){
-      $scope.tab1_checked=false;  
-      $scope.tab2_checked=true;  
-      $scope.tab3_checked=false;  
-      $scope.curtab="tab2";  
-      $scope.tab1_color="";   
-      $scope.tab2_color={color:'blue'};  
-      $scope.tab3_color="";  
-      GetPatientsbyStatus(2);
-      $scope.newPatientIcon=false; 
-  }
-  else{
-      $scope.tab1_checked=false;  
-      $scope.tab2_checked=false;  
-      $scope.tab3_checked=true;  
-      $scope.curtab="tab3";  
-      $scope.tab1_color="";   
-      $scope.tab2_color="";  
-      $scope.tab3_color={color:'blue'};  
-      GetPatientsbyStatus(3);
-      $scope.newPatientIcon=false; 
-  }
-
+      $scope.tab1_checked=true; 
  //tab切换列表 显示或隐藏
  $scope.sel_tab = function(vtab) {  
    if($scope.curtab!=vtab) 
